95eb6ad88a7f691f8e271a38aace9d717611ba72
Callumgander <callumgander@hotmail.com>
2020-02-04 11:54:17 +0000

    Delete isIPv4Address.js

--- isIPv4Address.js
+++
@@ -1,9 +0,0 @@
-// isIPv4Address codesignal
-
-// not mine
-function isIPv4Address(s) {
-  var r = s.split(".");
-  return (
-    r.length === 4 && r.every(x => x != "" && !isNaN(x) && x >= 0 && x < 256)
-  );
-}
